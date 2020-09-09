57fd7fa37706e14882ce11b36b8fa81695fe331d
373watohk <e185720@373watohk.local>
2020-09-09 17:16:43 +0900

    apple count

---
+++ main/2/src/main/java/3/ApplesCount.java
@@ -0,0 +1,10 @@
+//package 1;
+import java.util.Scanner;
+public class ApplesCount {
+    int applenum() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("りんごいくつ買いますか？: ");
+        int num2 = scanner.nextInt();
+        return num2;
+    }
+}
