a09814cb2bca20c97b0092e2f7a3f6e334f2a979
ISSEI510-1217 <e185729@ie.u-ryukyu.ac.jp>
2020-09-09 22:35:07 +0900

    update Main.java

--- main/2/src/main/java/3/Main.java
+++ main/2/src/main/java/3/Main.java
@@ -2,10 +2,13 @@
 //package 1;
 public class Main {
     public static void main(String[] args) {
-        CaluculateApple calapp = new CaluculateApple();
-        ApplesCount appc = new ApplesCount();
+        //issue1
         //Recieved100 re100 = new Recieved100();
         //re100.buy_apple();
+
+        //issue2
+        ApplesCount appc = new ApplesCount();
+        CaluculateApple calapp = new CaluculateApple();
         calapp.applesumvalue(appc.applenum());
     }
 }
